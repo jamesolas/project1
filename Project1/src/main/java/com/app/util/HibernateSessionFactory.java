284c845c698939a10c3678311ca9ac2824c0cc28
James Olas <jamesolas@gmail.com>
2021-03-02 13:57:08 -0800

    database credentials

--- Project1/src/main/java/com/app/util/HibernateSessionFactory.java
+++ Project1/src/main/java/com/app/util/HibernateSessionFactory.java
@@ -11,9 +11,12 @@ public class HibernateSessionFactory {
 	public static Session getSession() {
 		if(sessionFactory == null) {
 			sessionFactory = new Configuration().configure()
-					.setProperty("hibernate.connection.url", System.getenv("dburl"))
-					.setProperty("hibernate.connection.username", System.getenv("dbusername"))
-					.setProperty("hibernate.connection.password", System.getenv("dbpassword"))
+					.setProperty("hibernate.connection.url", "jdbc:postgresql://database-1.cqrofvu1umtq.us-west-1.rds.amazonaws.com:5432/")
+					.setProperty("hibernate.connection.username", "postgres")
+					.setProperty("hibernate.connection.password","password")
+					
+//					.setProperty("hibernate.connection.username", System.getenv("dbusername"))
+//					.setProperty("hibernate.connection.password", System.getenv("dbpassword"))
 					.buildSessionFactory();
 		}
 		return sessionFactory.getCurrentSession();
